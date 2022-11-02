70ed8f8661b00af0f643ccbfe1c0cea1beade8aa
Suman raj <raj_suman@hotmail.com>
2022-11-02 20:50:11 +0530

    final commit

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./header";
+import Footer from "./footer";
+import Text from "./text";
+
+function App(){
+    return ( 
+    <div>
+    <Header />
+    <Text />
+    <Footer />
+    </div>
+    );
+}
+export default App;
